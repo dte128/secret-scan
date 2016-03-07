e3dc6cc0813082aadc25156d215f6b12d4211c0b
Jon Heise <j.heise@gmail.com>
2016-03-06 23:40:36 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+#Secret-Scan
+---
+
+Recursively parse a directory for passwords and private keys
+
+```
+secret-scan ~/.ssh
+
+/home/user/.ssh/id_dsa -----BEGIN DSA PRIVATE KEY-----
+
+/home/user/.ssh/id_rsa -----BEGIN RSA PRIVATE KEY-----
+```
---
+++ secret-scan.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "os"
+    "path/filepath"
+    "regexp"
+    "sync"
+
+    "github.com/alecthomas/kingpin"
+)
+
+var (
+    start = kingpin.Arg("dir", "Directory to start processing in").String()
+    concur = kingpin.Flag("concurrency", "Number of concurrent threads").Default("1").Short('c').Int()
+    wg sync.WaitGroup
+    file_list chan string
+    )
+
+func walkfunc(path string, _ os.FileInfo, _ error) error {
+    file_list <-path
+
+    return nil
+}
+
+func file_processor(){
+    defer wg.Done()
+    for i := range file_list{
+        err := scanfile(i)
+        if err != nil {
+            panic(err)
+        }
+    }
+}
+
+func activate_processor(){
+    wg.Add(1)
+    go file_processor()
+}
+
+func scanfile(path string) error {
+    //fmt.Printf("Scanning %s...\n", path)
+    fp, err := os.Open(path)
+
+    if err != nil {
+        fmt.Println(err)
+        //os.Exit(1)
+    }
+
+    defer fp.Close()
+
+    reader := bufio.NewReader(fp)
+    scanner := bufio.NewScanner(reader)
+
+    for scanner.Scan() {
+        current := scanner.Text()
+        match, _ := regexp.MatchString("-{5}BEGIN [RD]SA PRIVATE KEY-{5}", current)
+        if match {
+            fmt.Printf("%s\t%s\n", path, current)
+        }
+    }
+
+    return nil
+}
+
+func main(){
+    kingpin.Parse()
+
+    var starting string
+
+    if len(*start) > 0 {
+        starting = *start
+    } else {
+        cwd, err := os.Getwd()
+        if err != nil {
+            panic(err)
+        }
+        starting = cwd
+    }
+    file_list = make( chan string, *concur)
+
+    for i :=0; i < *concur; i++ {
+        activate_processor()
+    }
+
+
+    filepath.Walk(starting, walkfunc)
+    close(file_list)
+}
